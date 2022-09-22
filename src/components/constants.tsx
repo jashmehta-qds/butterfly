9021266f1e46790478c9c0a0e90b84f01426bcb0
jash <jash@live.in>
2022-09-22 23:47:07 +0530

    updated comments
    Signed-off-by: jash <jash@live.in>

--- src/components/constants.tsx
+++ src/components/constants.tsx
@@ -9,8 +9,8 @@ export const CONTENT = {
           <b>Butterfly Bubble</b>, an <b>art</b> collection by the artist{" "}
           <b>Snoozed Sneeze</b> comprises <b>33 butterflies </b> within their
           respective bubbles, oblivious to their surroundings, each in a{" "}
-          <b>24 x 24 pixel grid.</b> This genesis collection is tokenised
-          complete on-chain and the artist invites you to take a leap of faith
+          <b>24 x 24 pixel grid.</b> This genesis collection is tokenized
+          completely on-chain and the artist invites you to take a leap of faith
           into the Butterfly Bubble ecosystem on the Ethereum blockchain (PoS).
         </p>
         <p>
@@ -46,7 +46,7 @@ export const CONTENT = {
       <>
         <p>
           {" "}
-          <b>Snoozed Sneeze (SNZ) </b>, is a <b>reactionary artist </b> from
+          <b>Snoozed Sneeze (SNZ) </b>, is a <b>reactionary artist </b>from
           Mumbai, India. His work largely focuses on the <b>intangible </b> as
           he adopts and aborts various ways to communicate the same. Submerged
           within white lies, he finds his most honest moments as those spent{" "}
@@ -56,11 +56,9 @@ export const CONTENT = {
           zeitgeist, he pivoted towards art for a more genuine and autonomous
           voice as a creator. <b>Scale, composition</b> and <b>materiality</b>{" "}
           informing his method, he transitioned to art through a few years of{" "}
-          <b>mural-making</b> and
-          <b>street art</b> along the western coast and a few{" "}
-          <b>Himalayan valleys in India.</b>
-          <br />
-          The flame of democratizing art burned bright within him.
+          <b>mural-making</b> and <b>street art</b> along the{" "}
+          <b>western coast</b> and a few <b>Himalayan valleys in India.</b> The{" "}
+          <b>flame of democratizing art burned bright within him.</b>
         </p>
         <p>
           He worked for an NGO’s initiative called ‘Chal Rang De’ (translates to
@@ -72,51 +70,47 @@ export const CONTENT = {
         </p>
         <p>
           Painting these walls as <b>massive canvasses</b> was fundamental to
-          the
-          <b>maturation</b> of SNZ’s art practice{" "}
+          the <b>maturation</b> of SNZ’s art practice{" "}
           <b>without compromising his innocence</b> in confronting ideas in a
           playful manner. His <b>visual vocabulary</b> reflects various{" "}
           <b>gradients</b> and <b>textures</b> from the{" "}
-          <b>vastness of seas, fields</b> and
-          <b>valleys</b> stippled with intricate forms of{" "}
-          <b>flowers, butterflies, birds</b>
-          and <b>people</b>. Since childhood, he was curious about{" "}
+          <b>vastness of seas, fields</b> and <b>valleys</b> stippled with
+          intricate forms of <b>flowers, butterflies, birds</b> and{" "}
+          <b>people</b>. Since childhood, he was curious about{" "}
           <b>metamorphosis</b> and the <b>complete disintegration</b> that a
           butterfly goes through as it evolves from a pupa. It offered him
-          flexibility growing up in
+          flexibility growing up in{" "}
           <b>
             disintegrating and reintegrating his conviction from time to time.
           </b>
         </p>
         <p>
-          Shortly after he collected his first NFT by an artist named{" "}
-          <b>NFN Kalyan </b>
-          in <b>July 2020 </b>, he returned to Mumbai.
-          <br />
-          SNZ credits NFN for playing a pivotal role in his own creative
-          journey.Anticipating a <b>paradigm shift</b>, words such as{" "}
+          In <b>July 2020</b> SNZ collected his first NFT by an artist named{" "}
+          <b>NFN Kalyan</b> whom he credits for playing a pivotal role in his
+          creative journey. Anticipating a <b>paradigm shift</b>, words such as{" "}
           <b>
             ambition, innovation, collaboration, autonomy, longevity,
             transparency
           </b>{" "}
           and such framed the foundation for the early conversation about{" "}
           <b>decentralized creation.</b> It was also when he adopted the
-          pseudonym <b>‘Snoozed Sneeze’</b>
-          as it is medically detrimental to snooze a sneeze - akin to
-          <b>withholding truth</b>. Few other human reactions are as genuine
-          than a sneeze. He longs for <b>honest discourse </b> through his work.
+          pseudonym <b>‘Snoozed Sneeze’</b> as it is medically detrimental to
+          snooze a sneeze - akin to <b>withholding truth</b>. Few other human
+          reactions are as genuine than a sneeze. He longs for{" "}
+          <b>honest discourse </b> through his work.
         </p>
         <p>
           {" "}
           <b>‘Time’ </b> plays an intriguing role in the overarching
           decentralized experience and in creation. Through SNZ’s earlier work
           tokenized as NFTs, he dabbles with ideas such as the{" "}
-          <b>inevitability of time </b> (‘Patients of Time’ - April 2021),{" "}
-          <b>identifying ourselves </b> (‘OG SUGGA’ September 2020,
-          ‘Epigenetics’ - April 2021) and <b>acknowledging our dissonance </b>{" "}
-          as we spin up our webs of perception and find ourselves trapped in
-          them (‘RISKY BIZKITS’ - September 2020, ‘MiNES FOR MiNDS’ - December
-          2021).
+          <b>inevitability of time </b> (<b>‘Patients of Time’ </b>- April
+          2021), <b>identifying ourselves </b> (<b>‘OG SUGGA’</b> September
+          2020,
+          <p>‘Epigenetics’ </p>- April 2021) and{" "}
+          <b>acknowledging our dissonance </b> as we spin up our webs of
+          perception and find ourselves trapped in them (<b>‘RISKY BIZKITS’</b>{" "}
+          - September 2020, <b>‘MiNES FOR MiNDS’</b> - December 2021).
         </p>
       </>
     ),
@@ -131,13 +125,13 @@ export const CONTENT = {
       <>
         <p>
           This art collection shall evolve over <b>8 years</b> contextualizing
-          his perception and art practice as he summarizes his experience with{" "}
-          <b>conviction.</b> The collection is an attempt at encouraging its
-          collectors to <b>imprint memorable moments</b> over these years as the
-          art continues to compound.
+          SNZ's perception and art practice as he summarizes his experience with
+          the <b>power of conviction.</b> The collection is an attempt at
+          encouraging its collectors to <b>imprint memorable moments</b> over
+          these years as the art continues to compound.
         </p>
         <p>
-          SNZ finds human nature to be <b>inevitably pendulous </b> due to our
+          SNZ finds human nature to be <b>inevitably pendulous </b>due to our
           conception of numerous options in any situation, synonymous to a{" "}
           <b>
             pendulum bob swinging in circular motion creating an illusion of
