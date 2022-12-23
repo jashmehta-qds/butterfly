26d76ee228a1aa8aa14b00e4346a662926ff6eb9
jash <jash@live.in>
2022-12-23 16:41:23 +0530

    updated content
    Signed-off-by: jash <jash@live.in>

--- src/components/constants.tsx
+++ src/components/constants.tsx
@@ -6,12 +6,12 @@ export const CONTENT = {
     BODY: (
       <>
         <p>
-          <b>Butterfly Bubble</b>, an <b>art</b> collection by the artist{" "}
-          <b>Snoozed Sneeze</b> comprises <b>33 butterflies </b> within their
-          respective bubbles, oblivious to their surroundings, each in a{" "}
-          <b>24 x 24 pixel grid.</b> This genesis collection is tokenized
-          completely on-chain and the artist invites you to take a leap of faith
-          into the Butterfly Bubble ecosystem on the Ethereum blockchain (PoS).
+          , an <b>art</b> collection by the artist <b>Snoozed Sneeze</b>{" "}
+          comprises <b>33 butterflies </b> within their respective bubbles,
+          oblivious to their surroundings, each in a This genesis collection is
+          tokenized completely on-chain and the artist invites you to take a
+          leap of faith into the Butterfly Bubble ecosystem on the Ethereum
+          blockchain (PoS).
         </p>
         <p>
           Each butterfly enables its collector to receive{" "}
@@ -22,13 +22,28 @@ export const CONTENT = {
           This would make them a <b>total of 14 butterflies per set </b>, each
           butterfly representing a day in the average
           <b> lifespan of a butterfly.</b>
+          <b>Butterfly Bubble</b>, an art collection by artist{" "}
+          <b>Snoozed Sneeze</b> comprises
+          <b>33</b> butterflies, each created on a <b>24 x 24</b> pixel grid.
+          The artist invites you into the Butterfly Bubble ecosystem through the{" "}
+          <b>Genesis</b>
+          collection, tokenized fully on the <b>
+            Ethereum blockchain (PoS).
+          </b>{" "}
+          Each butterfly will evolve through{" "}
+          <b>13 intermittent airdrops every 8 months</b>
+          and <b>only</b> those collectors who <b>own the genesis butterfly</b>{" "}
+          will be eligible to continue receiving them. <b>Minted and stored</b>{" "}
+          under a separate collection called <b>Butterfly Bubble: Phases</b> on
+          Inter Planetary File System <b>(IPFS)</b>, at the end of <b>8</b>{" "}
+          years, every collector will have <b>a set of 14 butterflies</b>, each
+          representing <b>a day in the average lifespan of a butterfly.</b>
         </p>
         <p>
           With each airdrop, the <b>pixel grid </b> that the butterfly and
-          bubble are created on <b>grows larger </b>, creating a more vivid and
-          denser visual, <b>allegorizing the maturation of a butterfly</b>. The
-          only prerequisite to receive these airdrops is for a collector to own
-          a genesis butterfly.
+          bubble are created on shall <b>grows larger</b>, creating{" "}
+          <b>a more vivid visual</b>,allegorizing the{" "}
+          <b>maturation of a butterfly.</b>
         </p>
       </>
     ),
@@ -51,24 +66,33 @@ export const CONTENT = {
           he adopts and aborts various ways to communicate the same. Submerged
           within white lies, he finds his most honest moments as those spent{" "}
           <b>observing, absorbing and reacting </b>
-          to the world through his art. Once his prospects with architecture
-          seemed limited by the form of design solutions necessary for the
-          zeitgeist, he pivoted towards art for a more genuine and autonomous
-          voice as a creator. <b>Scale, composition</b> and <b>materiality</b>{" "}
-          informing his method, he transitioned to art through a few years of{" "}
-          <b>mural-making</b> and <b>street art</b> along the{" "}
-          <b>western coast</b> and a few <b>Himalayan valleys in India.</b> The{" "}
-          <b>flame of democratizing art burned bright within him.</b>
+          to the world through his art.
         </p>
         <p>
-          He worked for an NGO’s initiative called ‘Chal Rang De’ (translates to
-          ‘Let us color’), painting <b>wall art in a slum area </b>
-          and a <b>police headquarter </b> in Mumbai, as well as co-creating
-          what was considered the{" "}
-          <b>third longest street art in India at the time (300ft x 15ft) </b>
-          for <b>Reliance </b> at the <b>Mumbai Metro rail yard</b>.
+          Once his prospects with architecture seemed limited by the form of
+          design solutions necessary for the zeitgeist, he pivoted towards art
+          for a more genuine and autonomous voice as a creator.{" "}
+          <b>Scale, composition</b> and <b>materiality</b> informing his method,
+          he transitioned to art through a few years of <b>mural-making</b> and{" "}
+          <b>street art</b> along the <b>western coast</b> and a few{" "}
+          <b>Himalayan valleys in India.</b> The{" "}
+          <b>flame of democratizing art burned bright within him,</b> while he
+          worked with NGOs in painting street art for{" "}
+          <b>slum areas, local police headquarters</b>, as well as co-creating
+          what was considered the <b>third longest street art in India</b> at
+          the time in 2017 (300ft x 15ft) for Reliance at the Mumbai Metro rail
+          yard.
         </p>
         <p>
+          His visual vocabulary reflects various <b>gradients and textures from the
+          vastness of seas, fields and valleys stippled with intricate forms of
+          flowers, butterflies, birds and people. ‘Metamorphosis’</b>  and the
+          <b>complete disintegration</b> that a butterfly goes through to merely exist
+          for a short span of time nurtured SNZ’s curiosity as a child. He
+          believes it offered him flexibility growing up in <b>disintegrating and
+          reintegrating his conviction from time to time.</b>
+        </p>
+        {/* <p>
           Painting these walls as <b>massive canvasses</b> was fundamental to
           the <b>maturation</b> of SNZ’s art practice{" "}
           <b>without compromising his innocence</b> in confronting ideas in a
@@ -98,7 +122,7 @@ export const CONTENT = {
           snooze a sneeze - akin to <b>withholding truth</b>. Few other human
           reactions are as genuine than a sneeze. He longs for{" "}
           <b>honest discourse </b> through his work.
-        </p>
+        </p>*/}
         <p>
           {" "}
           <b>‘Time’ </b> plays an intriguing role in the overarching
@@ -111,7 +135,7 @@ export const CONTENT = {
           <b>acknowledging our dissonance </b> as we spin up our webs of
           perception and find ourselves trapped in them (<b>‘RISKY BIZKITS’</b>{" "}
           - September 2020, <b>‘MiNES FOR MiNDS’</b> - December 2021).
-        </p>
+        </p> 
       </>
     ),
     FOOTER: [
@@ -124,32 +148,36 @@ export const CONTENT = {
     BODY: (
       <>
         <p>
-          This art collection shall evolve over <b>8 years</b> contextualizing
-          SNZ's perception and art practice as he summarizes his experience with
-          the <b>power of conviction.</b> The collection is an attempt at
-          encouraging its collectors to <b>imprint memorable moments</b> over
-          these years as the art continues to compound.
+          This art collection shall evolve over 8 years,{" "}
+          <b>
+            contextualizing SNZ's experience of the powers of human conviction.
+          </b>{" "}
+          He finds human nature to be inevitably pendulous while we’re
+          distracted by numerous tangential options, akin to a pendulum bob
+          swinging in circular motion creating an illusion of forward momentum.
+          In truth, we’re{" "}
+          <b>
+            simply looping as we remain suspended by several limiting forces
+            beyond our comprehension.
+          </b>
         </p>
         <p>
-          SNZ finds human nature to be <b>inevitably pendulous </b>due to our
-          conception of numerous options in any situation, synonymous to a{" "}
-          <b>
-            pendulum bob swinging in circular motion creating an illusion of
-            forward momentum{" "}
-          </b>
-          when instead we may be looping while we remain suspended by several
-          limiting forces beyond our comprehension.
-          <br /> Imprisoned by our freedom of choice, we repeat our mistakes,
-          trade off the pain for a bit of pleasure, justifying it all as we run
-          through our mind maze.
+          Conviction leads to the <b>creation and inflation</b> of several
+          bubbles. We have a proclivity towards{" "}
+          <b>preventing them from popping</b> and simply{" "}
+          <b>wishing them into evolving</b> through <b>social engineering.</b>{" "}
+          Imprisoned by our freedom of choice, we repeat our mistakes, trade off
+          the pain for a bit of pleasure, justifying it all as we run through
+          our mind maze.
         </p>
         <p>
-          {" "}
-          <b>
-            Buoyant with hope and anchored by regrets, he wishes his work
-            contributes to contemporary voices about the conformity of culture
-            and meaning or the cultivation of the same.{" "}
-          </b>
+          The artist encourages each collector to{" "}
+          <b>imprint memorable moments</b> over these years as the art continues
+          to compound, to challenge the{" "}
+          <b>digital drain of memories caused by overwhelming options.</b>{" "}
+          Buoyant with hope and anchored by regrets, he wishes his work
+          contributes to contemporary voices about the conformity of culture and
+          meaning or the cultivation of the same.
         </p>
       </>
     ),
